29f1129c38ae56e347b628025ca9d1bcce9f3dbc
Welson <kcwelson14@example.com>
2025-12-15 01:34:52 +0100

    Login implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RegisterSelect from "./pages/RegisterSelect";
 import RegisterCustomer from "./pages/RegisterCustomer";
 import RegisterBusiness from "./pages/RegisterBusiness";
+import Login from "./pages/LoginPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/register" element={<RegisterSelect />} />
       <Route path="/register/customer" element={<RegisterCustomer />} />
       <Route path="/register/business" element={<RegisterBusiness />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import API from "../api/auth";
+
+export default function Login() {
+  const [form, setForm] = useState({ email: "", password: "" });
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+
+//   useEffect(() => {
+//         if (localStorage.getItem("access")) {
+//             navigate("/");
+//         }
+//     }, [navigate]);
+
+  const handleChange = (e) =>
+    setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await API.post("login/", form);
+      localStorage.setItem("access", res.data.tokens.access);
+      localStorage.setItem("refresh", res.data.tokens.refresh);
+      setMessage("Logged in sucessfully.");
+    } catch (err) {
+        if (err.response?.status === 403) {
+            setMessage("Please verify your email first.");
+        } else {
+            setMessage("Invalid credentials or server error.");
+        }
+    }
+  };
+
+  return (
+    <div className="auth-container">
+      <h2>Login</h2>
+
+      {/* Normal login form */}
+      <form onSubmit={handleSubmit}>
+        <input
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        <button type="submit">Login</button>
+      </form>
+
+      <hr />
+
+      <p>{message}</p>
+    </div>
+  );
+}
